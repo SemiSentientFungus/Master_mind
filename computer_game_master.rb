3693edc42a663dbf502e94e29b5b9d0280549317
Matt <semisentientfungus@gmail.com>
2023-02-03 16:51:43 +0200

    add random code creation

--- computer_game_master.rb
+++ computer_game_master.rb
@@ -5,4 +5,12 @@ class ComputerGameMaster
   def initialize
     puts 'this is a computer game master!'
   end
+
+  def code
+    code = []
+    4.times do
+      code.push(%w[red green blue orange purple yellow].sample)
+    end
+    code
+  end
 end
