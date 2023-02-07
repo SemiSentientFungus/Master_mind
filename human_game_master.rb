cf09523f174a489f56c9fa5bec6a39bc5921f402
Matt <semisentientfungus@gmail.com>
2023-02-07 18:42:16 +0200

    add manual code generation

--- human_game_master.rb
+++ human_game_master.rb
@@ -3,6 +3,15 @@
 # class to take a human input and rate code attempts by the solver classes
 class HumanGameMaster
   def initialize
-    puts 'Hi! I\'m the Human Game Master'
+    @colours = %w[red orange yellow green blue purple]
+    @generated = ['empty']
+  end
+
+  def code
+    until (@generated - @colours).empty?
+      puts "Your choices are #{@colours}"
+      @generated = [gets.chomp, gets.chomp, gets.chomp, gets.chomp]
+    end
+    @generated
   end
 end
